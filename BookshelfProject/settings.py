b3ea5d2f3966ef36f0566fe2900c8a1ee36aeb41
alirezaseif20 <alirezaariye@gmail.com>
2024-07-04 15:26:58 +0330

    feature: add api app and rest_framework

--- BookshelfProject/settings.py
+++ BookshelfProject/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'books',
+    'api.apps.ApiConfig',
+    'books.apps.BooksConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
