5f5204d263d9d99224738a53d8d326293b1b4737
alirezaseif20 <alirezaariye@gmail.com>
2024-07-05 11:20:17 +0330

    feature: add tests for database

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
-
+from .models import Book
 # Create your tests here.
+
+class BookModelTest(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls):
+        Book.objects.create(title = 'my title', author = 'my author', code = '1234')
+        
+    def test_title_ccontent(self):
+        book = Book.objects.get(id=1)
+        title = f'{book.title}'
+        self.assertEqual(title, 'my title')
+        
+    def test_author_ccontent(self):
+        book = Book.objects.get(id=1)
+        author = f'{book.author}'
+        self.assertEqual(author, 'my author')
+        
+    def test_author_ccontent(self):
+        book = Book.objects.get(id=1)
+        code = f'{book.code}'
+        self.assertEqual(code, '1234')
